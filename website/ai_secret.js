a4ac1ff930b7b237d24757d84e8c68970fdea259
haji <hajjiplays@gmail.com>
2026-01-28 23:39:04 -0800

    dc

--- website/ai_secret.js
+++ website/ai_secret.js
@@ -8,6 +8,6 @@
 // Nebius TokenFactory (OpenAI-compatible)
 // Paste your key once here.
 // NOTE: This is loaded by the browser (client-side). Anyone who can load the site can view it.
-window.NEBIUS_API_KEY = 'PASTE_YOUR_KEY_HERE';
+window.NEBIUS_API_KEY = 'v1.CmMKHHN0YXRpY2tleS1lMDB3YzJhcmphZDlyYWphZHgSIXNlcnZpY2VhY2NvdW50LWUwMHZhY2FrMnpkY216cG0xNDIMCLaG7MsGEI-wxcQCOgsItomElwcQwLqZBkACWgNlMDA.AAAAAAAAAAF_tRJN6h-5Qic99apg-d2st3ACT73m9g-wsEEJXm72O69VoIXducC_7AwEe6np4DA7da11QIdLCgfb2oh8PccP';
 window.NEBIUS_BASE_URL = 'https://api.tokenfactory.nebius.com/v1/';
 window.NEBIUS_MODEL = 'nvidia/Nemotron-Nano-V2-12b';
